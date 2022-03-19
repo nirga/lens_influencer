e0b10b0f53dee4070d241a5195797b84d172af44
Norman Saade <norman.saade@gmail.com>
2022-03-19 08:59:48 +0100

    query example of lens

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
-function App() {}
+import { queryExample } from './queryExample';
+import './App.css';
+
+function App() {
+  queryExample()
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <h2>Apollo app using Lens 🚀</h2>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
