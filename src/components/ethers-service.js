a2fae34bd6a8ecadf29afcd6d844d3f2e73d57ec
Norman Saade <norman.saade@gmail.com>
2022-03-22 08:57:17 +0100

    wip login

--- src/components/ethers-service.js
+++ src/components/ethers-service.js
@@ -6,10 +6,11 @@ import { ethers } from 'ethers';
 export const ethersProvider = new ethers.providers.Web3Provider(window.ethereum);
 
 export const getAddress = async() => {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
   return accounts[0];
 }
 
 export const signText = (text) => {
+  console.log(ethersProvider)
   return ethersProvider.signMessage(text);
 }
