f09d4ac18487956f177c0d14135f0cc61cb96178
Nir Gazit <nir.gzt@gmail.com>
2022-03-26 13:39:07 +0300

    Fixes issues with TwitterVerifier and local imported ChainLink mocks

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,34 +9,6 @@ dotenv.config({ path: ".env" });
 const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-/*task("deploy", "Deploy Everything").setAction(async ({}, hre) => {
-  const lensHub = process.env.LENS_HUB;
-
-  console.log("Lens Hub:", lensHub);
-
-  await hre.run("compile");
-
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
-  // We get the contract to deploy
-  const HuntedAccountFactory = await hre.ethers.getContractFactory(
-    "HuntedAccountFactory"
-  );
-  const huntedAccountFactory = await HuntedAccountFactory.deploy(lensHub);
-
-  await huntedAccountFactory.deployed();
-
-  console.log(
-    "HuntedAccountFactory deployed to:",
-    huntedAccountFactory.address
-  );
-});
-*/
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -64,12 +36,6 @@ module.exports = {
     compilers: [
       {
         version: "0.8.10",
-      },
-      {
-        version: "0.6.6",
-      },
-      {
-        version: "0.4.24",
       },
     ],
   },
